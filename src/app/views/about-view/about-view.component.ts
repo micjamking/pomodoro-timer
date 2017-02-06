47b5e5f62f7996c5e0af1377b70973b1ee39736d
Mike King <yo@mikeking.io>
2017-02-06 01:00:06 -1000

    Cleaning up about page styles and adding transitions/animations

--- src/app/views/about-view/about-view.component.ts
+++ src/app/views/about-view/about-view.component.ts
@@ -5,10 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-view.component.html'
 })
 export class AboutViewComponent implements OnInit {
+  public currentYear: number;
 
   constructor() { }
 
   ngOnInit() {
+    let date = new Date();
+    this.currentYear = date.getFullYear();
   }
 
 }
